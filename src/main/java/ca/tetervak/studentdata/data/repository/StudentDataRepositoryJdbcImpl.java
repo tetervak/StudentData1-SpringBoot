de05ef0f0ffc7e72fb3d4b2ddd5198d92fd1ca7b
Alexander Tetervak <tetervak@sheridancollege.ca>
2022-07-10 19:07:17 -0400

    reformatted code

--- src/main/java/ca/tetervak/studentdata/data/repository/StudentDataRepositoryJdbcImpl.java
+++ src/main/java/ca/tetervak/studentdata/data/repository/StudentDataRepositoryJdbcImpl.java
@@ -1,8 +1,6 @@
 /* Alex Tetervak, Sheridan College, Ontario */
 package ca.tetervak.studentdata.data.repository;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -14,6 +12,8 @@ import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.stereotype.Repository;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.util.List;
 
 @Repository
